dbf0489da24f9f948483faaba200a06dd8e390b9
Srikumar K. S <srikumarks@gmail.com>
2012-10-19 11:39:36 +0530

    Added a naive tuner.

---
+++ tuner/tuner.js
@@ -0,0 +1,42 @@
+console.assert(org.anclab.steller);
+console.assert(srikumarks.audio.pitch);
+
+(function (Scheduler, AudioContext, pitch) {
+    var sh = new Scheduler(new AudioContext());
+    console.assert(sh.models);
+
+    var display = elements(['frequency', 'pitch', 'note', 'sig']);
+
+    var mic = sh.models.mic();
+    var spec = sh.models.spectrum(1024, 0.25);
+    mic.connect(spec);
+
+    var options = {lowSignificance: 8, highSignificance: 14};
+
+    spec.time.watch(function (t) {
+        var p = pitch(spec, options);
+        if (p) {
+            display.frequency.innerText = p.frequency;
+            display.pitch.innerText = p.pitch;
+            display.note.innerText = p.note.name + (p.error < 0 ? ' +' : ' ') + (-p.error) + ' cents';
+            display.sig.innerText = p.sig;
+        } else {
+            display.frequency.innerText = '';
+            display.pitch.innerText = '';
+            display.note.innerText = '';
+            display.sig.innerText = '< ' + options.lowSignificance;
+        }
+    });
+
+    // Start analysis.
+    sh.play(spec.start);
+
+    // Gets the DOM elements with the given ids.
+    function elements(ids) {
+        var nodes = {};
+        ids.forEach(function (id) {
+            nodes[id] = document.getElementById(id);
+        });
+        return nodes;
+    }
+}(org.anclab.steller.Scheduler, org.anclab.steller.AudioContext, srikumarks.audio.pitch));
