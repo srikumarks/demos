bdda2903e2f73bd63f8154bf687d94790e094374
Srikumar K. S <srikumarks@gmail.com>
2012-12-19 00:14:27 +0530

    Presentation

--- bhedam/bhedam.js
+++ bhedam/bhedam.js
@@ -128,7 +128,7 @@ function identify(db, svaras) {
 
 function showRaga(name) {
     var r = RagaDBKV[name];
-    E.raga_info.innerHTML = '<h4>' + r[0] + '</h4> arohanam: ' + r[1].join(', ') + '<br/>avarohanam: ' + r[2].join(', ');
+    E.raga_info.innerHTML = '<h3>' + r[0] + '</h3> arohanam: ' + r[1].join(', ') + '<br/>avarohanam: ' + r[2].join(', ');
 }
 
 function format(ragas, func) {
