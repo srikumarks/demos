e4d302cac77e58e993b5333320538a9cc4bda969
Srikumar K. S <srikumarks@gmail.com>
2013-07-16 10:58:32 +0530

    FEATURE: Added built-in support for using roman diacritic characters.

--- carnot/carnot_demo.js
+++ carnot/carnot_demo.js
@@ -5,20 +5,127 @@ window.requestAnimationFrame =  window.requestAnimationFrame ||
                                 function (fn) { setTimeout(fn, 15); };
 
 
+function Diacritics(cm, panel_id) {
+    var Suffix = {
+        normal: "",
+        overline: String.fromCharCode(parseInt("0304", 16)),
+        underline: String.fromCharCode(parseInt("0331", 16)),
+        overdot: String.fromCharCode(parseInt("0307", 16)),
+        overddot: String.fromCharCode(parseInt("0308", 16)),
+        breve: String.fromCharCode(parseInt("0301", 16)),
+        tilde: String.fromCharCode(parseInt("0303", 16)),
+        underdot: String.fromCharCode(parseInt("0323", 16)),
+        underddot: String.fromCharCode(parseInt("0324", 16))
+    };
+
+    var Ids = Object.keys(Suffix);
+
+    var elements = {};
+    Ids.forEach(function (k) {
+        elements[k] = document.getElementById(k);
+    });
+    
+    // A single string containing all the suffixes.
+    var Suffixes = Ids.map(function (k) { return Suffix[k]; }).join('');
+    var SuffixedRE = new RegExp('^[A-Za-z][' + Suffixes + ']?$');
+     
+    function accentableSelection(cm) {
+        if (!cm.doc.somethingSelected()) {
+            return false;
+        }
+
+        return SuffixedRE.test(cm.doc.getSelection());
+    }
+
+    // Displays the accented characters.
+    function showChars(c) {
+        if (c && !/\s/.test(c)) {
+            Ids.forEach(function (id) {
+                elements[id].innerText = c + Suffix[id];
+            });
+        }
+    }
+    
+    // Changes the diacritic for the current selection character.
+    // The diacritic is identified by its id name.
+    function setAccent(cm, id) {
+        var str = cm.doc.getSelection();
+
+        // Edit the string.
+        cm.doc.replaceSelection(str.charAt(0) + Suffix[id]);
+
+        flash(id);
+    }
+
+    function flash(id) {
+        elements[id].style.backgroundColor = '#8f8f8f';
+        setTimeout(function () {
+            elements[id].style.backgroundColor = '#cfcfcf';
+        }, 90);
+    }
+        
+    function init(cm, panel_id) {
+       var panel = document.getElementById(panel_id);
+        
+        var accentKeyMap = Object.create(CodeMirror.keyMap.default);
+        function makeAccentKeyHandler(key, id) {
+            return function (cm) {
+                if (!panel.hidden) {
+                    setAccent(cm, id);
+                } else {
+                    return CodeMirror.Pass;
+                }
+            };
+        }
+
+        accentKeyMap['1'] = makeAccentKeyHandler('1', 'normal');
+        accentKeyMap['2'] = makeAccentKeyHandler('2', 'overline');
+        accentKeyMap['3'] = makeAccentKeyHandler('3', 'underline');
+        accentKeyMap['4'] = makeAccentKeyHandler('4', 'breve');
+        accentKeyMap['5'] = makeAccentKeyHandler('5', 'tilde');
+        accentKeyMap['6'] = makeAccentKeyHandler('6', 'overdot');
+        accentKeyMap['7'] = makeAccentKeyHandler('7', 'overddot');
+        accentKeyMap['8'] = makeAccentKeyHandler('8', 'underdot');
+        accentKeyMap['9'] = makeAccentKeyHandler('9', 'underddot');
+
+        CodeMirror.keyMap['diacritics'] = accentKeyMap;
+        cm.setOption('keyMap', 'diacritics');
+
+        cm.on('cursorActivity', function (cm) {
+            if (accentableSelection(cm)) {
+                var xy = cm.cursorCoords();
+                panel.hidden = false;
+                panel.style.left = Math.max(0, Math.min(400, xy.left)) + 'px';
+                panel.style.bottom = (window.innerHeight - xy.top + 8) + 'px';
+                showChars(cm.doc.getSelection().charAt(0));
+            } else {
+                panel.hidden = true;
+            }
+        });
+
+        return Diacritics;
+    }
+    
+    init(cm, panel_id);
+
+    return Diacritics;
+}
+
 (function () {
     var Carnot = org.sriku.Carnot;
 
-    function getElements(array) {
-        var obj = {};
-        array.forEach(function (id) {
-            obj[id] = document.getElementById(id);
-        });
+    function getElements() {
+        var q = document.querySelectorAll('[id]');
+        var obj = {}, i, N;
+        for (i = 0, N = q.length; i < N; ++i) {
+            obj[q[i].getAttribute('id')] = q[i];
+        }
         return obj;
     }
 
-    var elements = getElements(['render_area', 'code', 'toolbar', 'print', 'newdoc']);
+    var elements = getElements();
     var key = 'sriku.org/demos/carnot_demo/notation';
-
+    
     elements.newdoc.onclick = function () {
         store(notation.getValue());
         notation.doc.setValue('');
@@ -42,8 +149,7 @@ window.requestAnimationFrame =  window.requestAnimationFrame ||
     window.addEventListener('resize', autoResizeCodeArea);
     notation.setOption('lineNumbers', false);
 
-    notation.on('cursorActivity', function (cm) {
-    });
+    Diacritics(notation, 'diacritics_panel');
 
     var codeChangeTime = Date.now();
     var debounceTime = 300;
